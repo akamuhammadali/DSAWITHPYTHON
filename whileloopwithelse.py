0adca5c73d68db57a756ed21ba13dfe7a29de1fe
akamuhammadali <122076231+akamuhammadali@users.noreply.github.com>
2023-03-17 11:58:16 +0500

    Delete whileloopwithelse.py

--- whileloopwithelse.py
+++
@@ -1,8 +0,0 @@
-total = 0
-number = int(input('Enter a number: '))
-# add numbers until number is zero
-while number != 0:
-    total += number    # total = total + number
-    # take integer input again
-    number = int(input('Enter a number: '))
-print('total =', total)
